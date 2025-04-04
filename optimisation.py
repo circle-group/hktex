Fix gradient bary->euclid, fix visualization of centers

optimisation.py
@@ -52,7 +52,7 @@ class OptimiseFixedHeatKernels:
             )
         self._idx_range = torch.arange(n_sources, device=device)
 
-        self._verts = torch.tensor(verts, device=device)
+        self._verts = torch.tensor(verts, device=device, dtype=torch.float)
         self._faces = torch.tensor(faces, device=device)
         self._diff_time_scaler_func = lambda x: 10 ** (4 * torch.tanh(x) - 2)
 
@@ -248,6 +248,7 @@ class OptimiseFixedHeatKernels:
         return v_colours, gt_colours, init_colours
 
     def render(self):
+        # TODO: Fix
         v_colours_buffer = torch.zeros(
             [self._n_sources, *self._verts.shape[:-1], self.kernel_dim],
             device=self.device,
@@ -292,8 +293,11 @@ class OptimiseFixedHeatKernels:
 
     @property
     def kernel_centres(self):
-        # TODO: Update
-        return self._verts[self._source_idxs]
+        kernel_vert_idx = self._faces[self.kernel_face_ids]
+        B, T = kernel_vert_idx.shape
+        kernel_vertx = self._verts[kernel_vert_idx.view(B * T)].view(B, T, -1)
+        barycentric_coords = self.kernel_locations
+        return utils.bary_to_cart_coords(barycentric_coords, kernel_vertx)
 
     @abstractmethod
     def _make_gt_colours(self):
@@ -548,7 +552,7 @@ if __name__ == "__main__":
         verts,
         faces,
         fnorm,
-        n_sources=128,
+        n_sources=256,
         k_eig=256,
         kernel_dim=32,
         fpath=mesh_path,
@@ -558,7 +562,7 @@ if __name__ == "__main__":
         vcols=vcols,
     )
 
-    v_colours, gt_colours, init_colours = optimisation.optimise(n_iter=1000)
+    v_colours, gt_colours, init_colours = optimisation.optimise(n_iter=1024)
     # torch.cuda.memory._dump_snapshot("memory_snapshot.pickle")
 
     v_colours = (v_colours * 255).to(dtype=torch.uint8)

tracer.py
@@ -56,8 +56,16 @@ class CPUGeodesicTracer(GeodesicTracer):
 
         new_coords = []
         for i in range(B):
+            # TODO: do the tangent_euclid in torch cuda
+            vert_ids = self._mesh.faces[face_id_np[i]]
+            coord_euclid = np.sum(
+                coord_np[i] * (self._mesh.vertices[vert_ids]), axis=0
+            ).reshape((1, 3))
+            tangent_euclid = np.sum(
+                tangent_np[i] * (coord_euclid - self._mesh.vertices[vert_ids]), axis=0
+            )
             trace_pts = self.tracer.trace_geodesic_from_face(
-                face_id_np[i], coord_np[i], tangent_np[i], self.max_iterations
+                face_id_np[i], coord_np[i], tangent_euclid, self.max_iterations
             )
             new_coords.append(trace_pts[-1, :])
         new_coords = np.stack(new_coords)

utils/torch_geodesics.py
@@ -2,7 +2,7 @@ import torch
 
 from .typing import *
 
-__all__ = ["uniform_sample_triangle"]
+__all__ = ["uniform_sample_triangle", "bary_to_cart_coords"]
 
 
 def uniform_sample_triangle(uniform_samples: Float[Tensor, "B 2"]):
@@ -10,3 +10,9 @@ def uniform_sample_triangle(uniform_samples: Float[Tensor, "B 2"]):
     b0 = 1 - su0
     b1 = uniform_samples[:, 1] * su0
     return torch.stack((b0, b1, 1 - b0 - b1), dim=1)
+
+
+def bary_to_cart_coords(
+    bary_coords: Float[Tensor, "B 3"], verts: Float[Tensor, "B 3 3"]
+):
+    return torch.einsum("ij,ijk->ik", bary_coords, verts)